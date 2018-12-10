87cee657cf75011a40b53c292a9fc9fb7310b8cc
Anurag <anurag.chowdhary@bigswitch.com>
2018-12-10 03:30:47 -0800

    Minor changes

--- system_setup.sh
+++ system_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y iptables-persistent openssh-server tmux nginx wget default-jre default-jvm
+sudo apt-get install -y iptables-persistent openssh-server tmux nginx wget default-jre default-jvm curl
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
@@ -15,3 +15,12 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda create -n py35 python=3.5
 conda env create -f environment.yml
 conda list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
